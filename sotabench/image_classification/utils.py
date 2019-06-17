7abac962bde40bf1b518718dea11a39bb10a8751
RJT1990 <rj-taylor@live.co.uk>
2019-06-17 16:29:45 +0100

    use torch.no_grad! all your memory are belong to us

--- sotabench/image_classification/utils.py
+++ sotabench/image_classification/utils.py
@@ -1,4 +1,5 @@
 import time
+import torch
 
 from sotabench.utils import AverageMeter, accuracy
 
@@ -11,21 +12,22 @@ def get_classification_metrics(model, test_loader, criterion, is_cuda=True):
 
     end = time.time()
 
-    for i, (input, target) in enumerate(test_loader):
+    with torch.no_grad():
+        for i, (input, target) in enumerate(test_loader):
 
-        if is_cuda:
-            target = target.cuda(non_blocking=True)
-            input = input.cuda()
+            if is_cuda:
+                target = target.cuda(non_blocking=True)
+                input = input.cuda()
 
-        # compute output
-        output = model(input)
-        loss = criterion(output, target)
-        prec1, prec5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.data.item(), input.size(0))
-        top1.update(prec1.item(), input.size(0))
-        top5.update(prec5.item(), input.size(0))
-        batch_time.update(time.time() - end)
-        end = time.time()
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+            prec1, prec5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.data.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+            top5.update(prec5.item(), input.size(0))
+            batch_time.update(time.time() - end)
+            end = time.time()
 
     return {
         'top_1_accuracy': top1.avg,
