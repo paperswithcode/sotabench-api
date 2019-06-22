a29456f4447bc4d9ccb2459f7caf8552f2d5e2a3
RJT1990 <rj-taylor@live.co.uk>
2019-06-22 17:37:44 +0100

    Fix training data zip location

--- sotabench/datasets/cityscapes.py
+++ sotabench/datasets/cityscapes.py
@@ -114,7 +114,7 @@ class Cityscapes(VisionDataset):
                              ' or "color"')
 
         if not os.path.isdir(self.images_dir) or not os.path.isdir(self.targets_dir):
-            image_dir_zip = self.images_dir + '_trainvaltest.zip'
+            image_dir_zip = os.path.join(self.root, 'leftImg8bit') + '_trainvaltest.zip'
 
             if self.mode == 'gtFine':
                 target_dir_zip = os.path.join(self.root, self.mode) + '_trainvaltest.zip'
