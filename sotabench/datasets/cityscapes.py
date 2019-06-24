b0c4476949c837a98fbd10f2006d1fc87937afc3
RJT1990 <rj-taylor@live.co.uk>
2019-06-24 14:17:57 +0100

    Add custom COCO dataset; add utilities for data processing

--- sotabench/datasets/cityscapes.py
+++ sotabench/datasets/cityscapes.py
@@ -6,6 +6,8 @@ from collections import namedtuple
 from torchvision.datasets.vision import VisionDataset
 from PIL import Image
 
+SEMANTIC_SEGMENTATION_IGNORE_LABEL = 255
+
 
 class Cityscapes(VisionDataset):
     """`Cityscapes <http://www.cityscapes-dataset.com/>`_ Dataset.
@@ -46,25 +48,25 @@ class Cityscapes(VisionDataset):
                                                      'has_instances', 'ignore_in_eval', 'color'])
 
     classes = [
-        CityscapesClass('unlabeled', 0, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('ego vehicle', 1, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('out of roi', 3, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('static', 4, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('dynamic', 5, 255, 'void', 0, False, True, (111, 74, 0)),
-        CityscapesClass('ground', 6, 255, 'void', 0, False, True, (81, 0, 81)),
+        CityscapesClass('unlabeled', 0, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (0, 0, 0)),
+        CityscapesClass('ego vehicle', 1, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (0, 0, 0)),
+        CityscapesClass('rectification border', 2, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (0, 0, 0)),
+        CityscapesClass('out of roi', 3, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (0, 0, 0)),
+        CityscapesClass('static', 4, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (0, 0, 0)),
+        CityscapesClass('dynamic', 5, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (111, 74, 0)),
+        CityscapesClass('ground', 6, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'void', 0, False, True, (81, 0, 81)),
         CityscapesClass('road', 7, 0, 'flat', 1, False, False, (128, 64, 128)),
         CityscapesClass('sidewalk', 8, 1, 'flat', 1, False, False, (244, 35, 232)),
-        CityscapesClass('parking', 9, 255, 'flat', 1, False, True, (250, 170, 160)),
-        CityscapesClass('rail track', 10, 255, 'flat', 1, False, True, (230, 150, 140)),
+        CityscapesClass('parking', 9, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'flat', 1, False, True, (250, 170, 160)),
+        CityscapesClass('rail track', 10, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'flat', 1, False, True, (230, 150, 140)),
         CityscapesClass('building', 11, 2, 'construction', 2, False, False, (70, 70, 70)),
         CityscapesClass('wall', 12, 3, 'construction', 2, False, False, (102, 102, 156)),
         CityscapesClass('fence', 13, 4, 'construction', 2, False, False, (190, 153, 153)),
-        CityscapesClass('guard rail', 14, 255, 'construction', 2, False, True, (180, 165, 180)),
-        CityscapesClass('bridge', 15, 255, 'construction', 2, False, True, (150, 100, 100)),
-        CityscapesClass('tunnel', 16, 255, 'construction', 2, False, True, (150, 120, 90)),
+        CityscapesClass('guard rail', 14, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'construction', 2, False, True, (180, 165, 180)),
+        CityscapesClass('bridge', 15, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'construction', 2, False, True, (150, 100, 100)),
+        CityscapesClass('tunnel', 16, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'construction', 2, False, True, (150, 120, 90)),
         CityscapesClass('pole', 17, 5, 'object', 3, False, False, (153, 153, 153)),
-        CityscapesClass('polegroup', 18, 255, 'object', 3, False, True, (153, 153, 153)),
+        CityscapesClass('polegroup', 18, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'object', 3, False, True, (153, 153, 153)),
         CityscapesClass('traffic light', 19, 6, 'object', 3, False, False, (250, 170, 30)),
         CityscapesClass('traffic sign', 20, 7, 'object', 3, False, False, (220, 220, 0)),
         CityscapesClass('vegetation', 21, 8, 'nature', 4, False, False, (107, 142, 35)),
@@ -75,8 +77,8 @@ class Cityscapes(VisionDataset):
         CityscapesClass('car', 26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
         CityscapesClass('truck', 27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
         CityscapesClass('bus', 28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
-        CityscapesClass('caravan', 29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
-        CityscapesClass('trailer', 30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
+        CityscapesClass('caravan', 29, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'vehicle', 7, True, True, (0, 0, 90)),
+        CityscapesClass('trailer', 30, SEMANTIC_SEGMENTATION_IGNORE_LABEL, 'vehicle', 7, True, True, (0, 0, 110)),
         CityscapesClass('train', 31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
         CityscapesClass('motorcycle', 32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
         CityscapesClass('bicycle', 33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
