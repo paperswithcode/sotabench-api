503145c3b2696d0d31c623724d1e549c2e3718c9
RJT1990 <rj-taylor@live.co.uk>
2019-06-29 13:22:10 +0100

    remove last / from data_root...

--- sotabench/semantic_segmentation/pascalvoc.py
+++ sotabench/semantic_segmentation/pascalvoc.py
@@ -15,7 +15,7 @@ class PASCALVOC:
 
     @classmethod
     def benchmark(cls, model, dataset_year='2007', input_transform=None, target_transform=None, transforms=None,
-                  model_output_transform=None, device: str = 'cuda', data_root: str = './.data/vision/voc/', num_workers: int = 4,
+                  model_output_transform=None, device: str = 'cuda', data_root: str = './.data/vision/voc', num_workers: int = 4,
                   batch_size: int = 128, num_gpu: int = 1, paper_model_name: str = None, paper_arxiv_id: str = None,
                   paper_pwc_id: str = None, pytorch_hub_url: str = None) -> BenchmarkResult:
 
