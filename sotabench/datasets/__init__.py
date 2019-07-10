e99b62df69ce5a6bf8da2b2b0c865151ca1a48bd
RJT1990 <rj-taylor@live.co.uk>
2019-07-10 11:38:45 +0100

    CoNLL 2003 english dataset

--- sotabench/datasets/__init__.py
+++ sotabench/datasets/__init__.py
@@ -2,4 +2,5 @@ from .ade20k import ADE20K
 from .camvid import CamVid
 from .cityscapes import Cityscapes
 from .coco import CocoDetection
+from .conll03 import CoNLL2003
 from .pascalcontext import PASCALContext
