09d3e96206bfa58477e779d5abb9a15225b61f46
RJT1990 <rj-taylor@live.co.uk>
2019-08-19 18:40:25 +0100

    add docs for decorator; increment version

--- sotabenchapi/core/inputs.py
+++ sotabenchapi/core/inputs.py
@@ -4,7 +4,12 @@ from sotabenchapi.core.results import BenchmarkResult
 
 
 def check_inputs(func):
-
+    """
+    A decorator for checking inputs to a benchmark method.
+    :param func: a benchmark method, e.g. ImageNet.benchmark(...)
+    :return: if regular evaluation, then func; if parameter check only then skips evaluation and returns inputs
+    in a BenchMarkResult object so they can be checked for correctness (e.g. if model name is correct)
+    """
     check_mode = os.environ.get("SOTABENCH_CHECK")
 
     def param_check_only(*args, **kwargs):
