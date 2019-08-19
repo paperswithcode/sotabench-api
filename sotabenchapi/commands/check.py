dae3bde99a3b32f30647cf5644655d741edeec6d
RJT1990 <rj-taylor@live.co.uk>
2019-08-19 14:10:38 +0100

    Add command line arguments for different checking modes

--- sotabenchapi/commands/check.py
+++ sotabenchapi/commands/check.py
@@ -12,8 +12,20 @@ from sotabenchapi.commands.utils import handle_errors
 
 @cli.command("check")
 @click.pass_obj
+@click.option("-cm", "--checkmeta",
+              is_flag=True,
+              default=False,
+              help="Checks the metadata, such as model names and arxiv paper ids, "
+                   "to ensure they are correct. Does not perform evaluation, "
+                   "but is a pure API call with the inputs. You can use this command to check "
+                   "input validity before submission ")
+@click.option("-cp", "--checkparams",
+              is_flag=True,
+              default=False,
+              help="Checks all parameters, including the model, by performing one batch of evaluation "
+                   "and ensuring things run. Afterwards will also check metadata - same checks as checkmeta")
 @handle_errors()
-def check(config: Config):
+def check(config: Config, checkparams: bool=False, checkmeta: bool = False):
     """Check if the benchmarking setup is correct."""
     cwd = Path(os.getcwd()).absolute()
 
@@ -25,9 +37,16 @@ def check(config: Config):
         click.secho("requirements.txt is missing.", fg="red")
         sys.exit(1)
 
+    if checkparams is True:
+        check_var = "params"
+    elif checkmeta is True:
+        check_var = "meta"
+    else:
+        check_var = "full"
+
     process = subprocess.Popen(
         [sys.executable, "sotabench.py"],
-        env={"SOTABENCH_CHECK": config.sotabench_check or "full"},
+        env={"SOTABENCH_CHECK": config.sotabench_check or check_var},
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
@@ -43,12 +62,3 @@ def check(config: Config):
         if stderr:
             click.secho(f"\nStderr:", fg="cyan")
             click.secho(stderr, fg="red")
-
-    # Check hashes
-    hashes = ["foo", "bar"]
-    client = Client(config)
-    result = client.check_run_hashes(hashes)
-    for h, exist in result.items():
-        click.secho(
-            f"Hash: `{h}` - {'exists' if exist else 'does not exist.'}"
-        )
